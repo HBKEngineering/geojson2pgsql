2b9572e10c836de3c8fb7a393129ab202059be9b
Brett Neese <brett@neese.rocks>
2018-06-07 15:07:45 -0700

    start making our own improvements

--- bin/geojson2pgsql.js
+++ bin/geojson2pgsql.js
@@ -3,14 +3,13 @@
 "use strict";
 
 var path = require("path"),
-    util = require("util");
+  util = require("util");
 
-var env = require("require-env"),
-    pg = require("pg");
-
-var optimist = require('optimist')
-      .usage("Usage: $0 [<options>] <GeoJSON file> [[schema.]<table>]"),
-    argv = optimist.argv;
+const { Pool, Client } = require("pg");
+var optimist = require("optimist").usage(
+    "Usage: $0 [<options>] <GeoJSON file> [[schema.]<table>]"
+  ),
+  argv = optimist.argv;
 
 if (argv._.length < 1) {
   optimist.showHelp();
@@ -19,40 +18,40 @@ if (argv._.length < 1) {
 
 var getFeatures = function(data) {
   switch (data.type) {
-  case "FeatureCollection":
-    return data.features;
+    case "FeatureCollection":
+      return data.features;
 
-  case "Feature":
-    return [data];
+    case "Feature":
+      return [data];
 
-  default:
-    throw new Error("Unsupported GeoJSON type: " + data.type);
+    default:
+      throw new Error("Unsupported GeoJSON type: " + data.type);
   }
 };
 
 var getType = function(val) {
-  switch (typeof(val)) {
-  case "number":
-    return "numeric";
+  switch (typeof val) {
+    case "number":
+      return "numeric";
 
-  case "string":
-    return "varchar";
+    case "string":
+      return "varchar";
 
-  default:
-    throw new Error("Unsupported type: " + typeof(val));
+    default:
+      throw new Error("Unsupported type: " + typeof val);
   }
 };
 
 var getDimensionality = function(geometry) {
   switch (geometry.type) {
-  case "LineString":
-    return geometry.coordinates[0].length;
+    case "LineString":
+      return geometry.coordinates[0].length;
 
-  case "Point":
-    return geometry.coordinates.length;
+    case "Point":
+      return geometry.coordinates.length;
 
-  default:
-    throw new Error("Unsupported GeoJSON geometry type: " + geometry.type);
+    default:
+      throw new Error("Unsupported GeoJSON geometry type: " + geometry.type);
   }
 };
 
@@ -60,38 +59,48 @@ var getGeometryType = function(geometry) {
   var dimensionality = getDimensionality(geometry);
 
   switch (dimensionality) {
-  case 2:
-    return geometry.type;
+    case 2:
+      return geometry.type;
 
-  case 3:
-    // TODO allow an option to make this '<type>M'
-    return geometry.type + "Z";
+    case 3:
+      // TODO allow an option to make this '<type>M'
+      return geometry.type + "Z";
 
-  case 4:
-    return geometry.type + "ZM";
+    case 4:
+      return geometry.type + "ZM";
 
-  default:
-    throw new Error("Unsupported number of dimensions: " + dimensionality);
+    default:
+      throw new Error("Unsupported number of dimensions: " + dimensionality);
   }
 };
 
 var asWKT = function(geometry, srid) {
   switch (geometry.type) {
-  case "LineString":
-    return util.format("SRID=%d;LINESTRING(%s)", srid, geometry.coordinates.map(function(x) {
-      return x.join(" ");
-    }).join(","));
+    case "LineString":
+      return util.format(
+        "SRID=%d;LINESTRING(%s)",
+        srid,
+        geometry.coordinates
+          .map(function(x) {
+            return x.join(" ");
+          })
+          .join(",")
+      );
 
-  case "Point":
-    return util.format("SRID=%d;Point(%s)", srid, geometry.coordinates.join(" "));
+    case "Point":
+      return util.format(
+        "SRID=%d;Point(%s)",
+        srid,
+        geometry.coordinates.join(" ")
+      );
 
-  default:
-    throw new Error("Unsupported GeoJSON geometry type: " + geometry.type);
+    default:
+      throw new Error("Unsupported GeoJSON geometry type: " + geometry.type);
   }
 };
 
 var source = argv._.shift(),
-    target = argv._.shift() || path.basename(source, path.extname(source));
+  target = argv._.shift() || path.basename(source, path.extname(source));
 
 // TODO special-case '-' for stdin
 var data = require(path.join(process.cwd(), source));
@@ -99,10 +108,12 @@ var data = require(path.join(process.cwd(), source));
 var sampleFeature = getFeatures(data)[0];
 
 var idType = getType(sampleFeature.id),
-    geometryType = getGeometryType(sampleFeature.geometry),
-    srid = 4326; // TODO make this configurable, sample it from the features list
+  geometryType = getGeometryType(sampleFeature.geometry),
+  srid = 4326; // TODO make this configurable, sample it from the features list
 
-var client = new pg.Client(env.require("DATABASE_URL"));
+const client = new Client({
+  connectionString: process.env.PG_CONNECTION_STRING
+});
 
 client.connect(function(err) {
   if (err) {
@@ -117,26 +128,31 @@ client.on("drain", function() {
 
 // TODO don't always do this
 client.query(util.format("DROP TABLE IF EXISTS %s", target));
-client.query(util.format("CREATE TABLE %s (id %s NOT NULL, properties JSON, geom GEOMETRY(%s, %d), PRIMARY KEY(id))",
-                                target, // TODO sanitize
-                                idType,
-                                geometryType,
-                                srid), function(err) {
-  if (err) {
-    console.warn(err);
+client.query(
+  util.format(
+    "CREATE TABLE %s (id %s NOT NULL, properties JSONB, geometry GEOMETRY(%s, %d), PRIMARY KEY(id))",
+    target, // TODO sanitize
+    idType,
+    geometryType,
+    srid
+  ),
+  function(err) {
+    if (err) {
+      console.warn(err);
+    }
   }
-});
+);
 
-var insertQuery = util.format("INSERT INTO %s (id, properties, geom) VALUES ($1, $2, $3)", target);
+var insertQuery = util.format(
+  "INSERT INTO %s (id, properties, geometry) VALUES ($1, $2, $3)",
+  target
+);
 
 getFeatures(data).forEach(function(feature) {
-  var params = [
-    feature.id,
-    feature.properties,
-    asWKT(feature.geometry, srid)
-  ];
+  var params = [feature.id, feature.properties, asWKT(feature.geometry, srid)];
 
   client.query(insertQuery, params, function(err) {
+    console.log(insertQuery);
     if (err) {
       console.warn(err);
     }
